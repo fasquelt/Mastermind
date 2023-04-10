6015e9d62808537ec81fdae761ac854c57acba0a
ricoupat <guillaume.bellone02@gmail.com>
2023-04-10 18:01:00 +0200

    Added an Enum to represent Mastermind's pimple by colors

---
+++ couleurs.c
@@ -0,0 +1,19 @@
+
+/*
+    Définition des 9 couleurs du Mastermind.
+*/
+/* 
+    Il est possible d'en rajouter, il faudra juste modifier les choix du joueur.
+    Cette modification ce fait depuis client.c.
+*/
+typedef enum picoColors {
+    Rouge = 0,
+    Bleue = 1,
+    Vert = 2,
+    Jaune = 3,
+    Violet = 4,
+    Orange = 5,
+    Marron = 6,
+    Rose = 7,
+    Flushia = 8
+} Picot;
